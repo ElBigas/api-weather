4bebdeb24e622f084d991d49c0c30ce135128927
Emanuel Evangelista <97486394+ElBigas@users.noreply.github.com>
2023-04-17 22:28:44 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Objeto para fazer as chamadas de API
 const api = {
     key: 'd8bfd2898285d74ba1e7eb88c842efa5',
     base: 'https://api.openweathermap.org/data/2.5/',
@@ -5,6 +6,7 @@ const api = {
     units: 'metric'
 }
 
+// Constantes para selecionar elementos do DOM
 const city = document.querySelector('.city')
 const date = document.querySelector('.date')
 const containerImg = document.querySelector('.container-img')
@@ -15,27 +17,45 @@ const weatherT = document.querySelector('.weather')
 const searchInput = document.querySelector('.form-control')
 const searchButton = document.querySelector('.btn')
 const lowHigh = document.querySelector('.low-high')
+const alertMessage = document.querySelector('.alert')
 
-//Adiciona um evento ao clicar no botão de pesquisar
+//Função que apaga input
+function erase() {
+    searchInput.value = ''
+}
+
+//Chama a função 'serchResults' ao clicar no botão de pesquisar
 searchButton.addEventListener('click', () => {
-    serchResults(searchInput.value)
+
+    let city = searchInput.value
+
+    serchResults(city.trim());
+
+    erase();
 })
 
-//Adiciona um evento ao apetar enter
+//Chama a função 'serchResults' ao apertar enter
 searchInput.addEventListener('keypress', e => {
     key = e.keyCode
 
     if (key === 13) {
-        serchResults(searchInput.value)
+        let city = searchInput.value
+
+        serchResults(city.trim());
+        erase();
     }
 })
+
+// Variável de controle para o timeout do erro
+let errorTimeout;
 
 //Função para ativar Geolocalização
 window.addEventListener('load', () => {
     if ('geolocation' in navigator) {
         navigator.geolocation.getCurrentPosition(setPosition, showError)
     } else {
-        alert('Navegador não suporta Geolocalização')
+        alertMessage.style.display = "block";
+        alertMessage.innerHTML = 'Navegador não suporta Geolocalização'
     }
 
     function setPosition(position) {
@@ -47,7 +67,20 @@ window.addEventListener('load', () => {
     }
 
     function showError(error) {
-        alert(`erro ${error.message}`)
+
+        // Cancela qualquer timeout anterior antes de configurar um novo
+        clearTimeout(errorTimeout);
+
+        alertMessage.style.display = "block";
+        alertMessage.innerHTML = `${error.message}`;
+
+        alertMessage.style.display = "block";
+        alertMessage.innerHTML = `${error.message}`;
+
+        // Chama a função para esconder o alerta depois de 5 segundos
+        errorTimeout = setTimeout(() => {
+            alertMessage.style.display = "none";
+        }, 5000);
     }
 })
 
@@ -58,13 +91,32 @@ function serchResults(city) {
     )
         .then(response => {
             console.log(response)
+            if (response.status === 404) {
+                throw new Error('Digite uma cidade válida')
+            }
+            if (response.status === 400) {
+                throw new Error('Campo vazio')
+            }
             if (!response.ok) {
-                throw new Error(`http error : status ${response.status}`)
+                throw new Error(`Erro HTTP: status ${response.status}`)
             }
             return response.json()
         })
+
         .catch(error => {
-            alert(error.message)
+            // Cancela qualquer timeout anterior antes de configurar um novo
+            clearTimeout(errorTimeout);
+
+            alertMessage.style.display = "block";
+            alertMessage.innerHTML = `Erro: ${error.message}`;
+
+            alertMessage.style.display = "block";
+            alertMessage.innerHTML = `Erro: ${error.message}`;
+
+            // Chama a função para esconder o alerta depois de 5 segundos
+            errorTimeout = setTimeout(() => {
+                alertMessage.style.display = "none";
+            }, 5000);
         })
         .then(response => {
             displayResults(response)
@@ -84,13 +136,23 @@ function coorResults(lat, long) {
             return response.json()
         })
         .catch(error => {
-            alert(error.message)
+            // Cancela qualquer timeout anterior antes de configurar um novo
+            clearTimeout(errorTimeout);
+
+            alertMessage.style.display = "block";
+            alertMessage.innerHTML = `error: ${error.message}`;
+
+            // Chama a função para esconder o alerta depois de 5 segundos
+            errorTimeout = setTimeout(() => {
+                alertMessage.style.display = "none";
+            }, 5000);
         })
         .then(response => {
             displayResults(response)
         })
 }
 
+//Função para exibir resultados vindos da API
 function displayResults(weather) {
     console.log(weather)
 
@@ -106,13 +168,14 @@ function displayResults(weather) {
     tempNumber.innerHTML = temperature
     tempUnit.innerHTML = '°C'
 
-    weatherT.innerHTML = weather.weather[0].description
+    weatherT.innerHTML = weather.weather[0].description.charAt(0).toUpperCase() + weather.weather[0].description.slice(1);
 
     lowHigh.innerHTML = `${Math.round(weather.main.temp_min)}°C / ${Math.round(
         weather.main.temp_max
     )}°C`
 }
 
+//Função para montar semana
 function dateBuilder(d) {
     let days = [
         'Domingo',
@@ -139,7 +202,7 @@ function dateBuilder(d) {
         'Dezembro'
     ]
 
-    let day = days[d.getDay()] //getDay: 0-6
+    let day = days[d.getDay()]
     let date = d.getDate()
     let month = months[d.getMonth()]
     let year = d.getFullYear()
